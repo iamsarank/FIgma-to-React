8e61561260ea1e2832f3c159f7f9ee949d2e6ff5
Saravanakumar <iamsarank@gmail.com>
2025-09-03 20:10:42 +0530

    tailwind css installed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1>Hello React</h1>
+      <h1 className="text-3xl font-bold underline text-center justify-center bg-red-600 text-white">Hello React</h1>
     </>
   )
 }
